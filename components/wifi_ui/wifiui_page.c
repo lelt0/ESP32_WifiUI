389306fff71db245670557d2658b8df1b0ee6da6
lelt0 <168340381+lelt0@users.noreply.github.com>
2026-04-05 20:33:57 +0900

    fix html style

--- components/wifi_ui/wifiui_page.c
+++ components/wifi_ui/wifiui_page.c
@@ -124,31 +124,30 @@ const char * html_head_template = R"(
 <title>%s</title>
 <style>
 * { box-sizing: border-box; }
-
 body { background: #FFF; color: #222; font-family: system-ui, -apple-system, sans-serif; line-height: 1.6; 
-  width: min(100vh, 100vw); margin: 0 auto; padding: 1em 1em;
+width: min(100vh, 100vw); margin: 0 auto; padding: 1em 1em;
 }
-
 h1, h2, h3, h4, h5, h6, p { margin: 0 0 0.8em 0; text-align: left; }
 
 button { display: block;
-  margin: 0.5em auto; padding: 12px 16px; border: none; border-radius: 8px;
-  font-size: 1rem; background: #2d6cdf; color: #fff; cursor: pointer;
+margin: 0.5em auto; padding: 0.6em 1.2em; border: none; border-radius: 0.5em;
+font-size: 1em; background: #2d6cdf; color: #fff; cursor: pointer;
 }
 button { width: 100%%;} @media (min-width: 600px) { button { width: auto; min-width: 100px; } }
 button:hover { background: #005fcc; } 
 
-img, video { display: block; 
-  margin: 10px auto; max-width: 100%%; max-height: 100vh; }
+img, video { display: block; margin: 1em auto; max-width: 100%%; max-height: 100vh; }
 
-.plot_container canvas { display: block; 
-  margin: 1em auto; 
-  background: #fff; border: 1px solid #eee;
-}
-
-.inline { display: inline-block;
-  margin: 5px; width: auto;
-}
+.plot_container canvas { display: block; margin: 1em auto; background: #fff; border: 1px solid #eee;}
+.inline { display: inline-block; margin: 0.5em; width: auto;}
+.wrap_text { white-space: pre-wrap; }
+.multi_input { font-family: inherit; font-size: inherit; line-height: inherit; width: 100%%; box-sizing: border-box; resize: none; overflow: hidden; min-height: 1.6em; padding: 0.5em; white-space: pre-wrap; word-break: break-all; border: 1px solid #ccc; border-radius: 0.5em; margin-bottom: -0.4em; }
+.single_input { font-family: inherit; font-size: inherit; line-height: inherit; width: 100%%; box-sizing: border-box; resize: none; overflow: hidden; min-height: 1.6em; padding: 0.5em; white-space: pre-wrap; word-break: break-all; border: 1px solid #ccc; border-radius: 0.5em; }
+.combo_input { width: 100%%; padding: 8px; box-sizing: border-box; }
+.combo_list { top: 100%%; left: 0; right: 0; border: 1px solid #ccc; border-top: none; overflow-y: auto; display: none; z-index: 1000; }
+.combo_item { padding: 6px 8px; cursor: pointer; }
+.combo_item:hover { background: #def; }
+.message_log {width: 100%%; height: 200px; border: 1px solid #ccc; resize: none; overflow-y: auto; word-break: break-all; overflow-wrap: break-word; }
 </style>
 <script>
 function fit_textarea_height(id){ t = document.getElementById(id); t.style.height = 'auto'; t.style.height = t.scrollHeight + 'px'; }
@@ -163,33 +162,33 @@ let ws = new WebSocket('ws://' + location.host + location.pathname + '/ws');
 let ws_actions = {};
 ws.binaryType = 'arraybuffer';
 ws.onmessage = function(evt) {
-    var data = new DataView(event.data);
-    let eid = data.getUint16(0, true);
-    if (eid in ws_actions) { ws_actions[eid](event.data.slice(2)); }
+ var data = new DataView(event.data);
+ let eid = data.getUint16(0, true);
+ if (eid in ws_actions) { ws_actions[eid](event.data.slice(2)); }
 };
 function cstr2str(array) {
-    const data = new Uint8Array(array);
-    const nulIndex = data.indexOf(0);
-    const slice = nulIndex >= 0 ? data.subarray(0, nulIndex) : data;
-    const text = new TextDecoder("utf-8").decode(slice);
-    return text;
+ const data = new Uint8Array(array);
+ const nulIndex = data.indexOf(0);
+ const slice = nulIndex >= 0 ? data.subarray(0, nulIndex) : data;
+ const text = new TextDecoder("utf-8").decode(slice);
+ return text;
 }
 function str2cstr(str) {
-    const encoder = new TextEncoder();
-    const bytes = encoder.encode(str);
-    const cstr = new Uint8Array(bytes.length + 1);
-    cstr.set(bytes, 0);
-    cstr[bytes.length] = 0; // NULL終端
-    return cstr;
+ const encoder = new TextEncoder();
+ const bytes = encoder.encode(str);
+ const cstr = new Uint8Array(bytes.length + 1);
+ cstr.set(bytes, 0);
+ cstr[bytes.length] = 0; // NULL終端
+ return cstr;
 }
 function ws_send_with_eid(eid, array) {
-    const header = new Uint8Array(2);
-    header[0] = eid & 0xff;
-    header[1] = (eid >> 8) & 0xff;
-    const payload = new Uint8Array(header.length + array.length);
-    payload.set(header, 0);
-    payload.set(array, header.length);
-    ws.send(payload);
+ const header = new Uint8Array(2);
+ header[0] = eid & 0xff;
+ header[1] = (eid >> 8) & 0xff;
+ const payload = new Uint8Array(header.length + array.length);
+ payload.set(header, 0);
+ payload.set(array, header.length);
+ ws.send(payload);
 }
 window.addEventListener("beforeunload", () => {
   if (ws.readyState === WebSocket.OPEN) {
